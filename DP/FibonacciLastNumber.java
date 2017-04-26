ce1ff368b3d4167dc30750d866ebf08fa624e4a7
Tejchava <tejchava@gmail.com>
2017-04-26 15:03:20 -0700

    Update FibonacciLastNumber.java

--- DP/FibonacciLastNumber.java
+++ DP/FibonacciLastNumber.java
@@ -4,21 +4,77 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+// public class Solution {
+//     public static void main(String[] args){
+//         Scanner in = new Scanner(System.in);
+//         int N = in.nextInt();
+//         int a = 0;
+//         int b = 1;
+//         if(N == 0)
+//             b = 0;
+//         N-=1;
+//         while(N-- > 0){
+//             int x = b;
+//             b = (a + b)%10;
+//             a = x;
+//         }
+        
+//         System.out.println(b);
+//     }
+// }
+
+//Above is one kind of slow solution.
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+
 public class Solution {
+    
+    public static int totalSum(int sum, long N, List<Integer> aL){
+        sum = (sum*((int)N/aL.size()))%10;
+        for(int i = 0; i <= N%aL.size(); i++)
+            sum = (sum + aL.get(i))%10;
+        return sum;
+    }
+    
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
+        long M = 0;
+        long N = in.nextLong();
         int a = 0;
         int b = 1;
-        if(N == 0)
-            b = 0;
-        N-=1;
-        while(N-- > 0){
+        int sum = 0;
+        int j = 1;
+        List<Integer> aL = new ArrayList<Integer>();
+        aL.add(a); aL.add(b);
+        while(j == 1){
             int x = b;
-            b = (a + b)%10;
+            b = (a+b)%10;
             a = x;
+            if (a == 0 && b == 1){
+                aL.remove(aL.size()-1);
+                break;
+            }else{
+                aL.add(b);
+            }
         }
-        
-        System.out.println(b);
+        if(N < aL.size()){
+            for(int i = (int)M; i <= N; i++)
+                sum = (sum + aL.get(i))%10;
+        }else{
+            for(int i = 0; i < aL.size(); i++)
+                sum = (sum + aL.get(i))%10;
+            int sum1 = sum;
+            sum = totalSum(sum, N, aL);
+            M -= 1;
+            sum -= totalSum(sum1, M, aL);
+            if(sum < 0)
+                sum+=10;
+        }
+        System.out.println(sum);
     }
 }
