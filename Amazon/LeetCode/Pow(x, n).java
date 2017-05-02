32f405ea65b80595ef4046f9512f24ac4e328cba
Tejchava <tejchava@gmail.com>
2017-05-01 19:00:36 -0700

    Create Pow(x, n).java

---
+++ Amazon/LeetCode/Pow(x, n).java
@@ -0,0 +1,14 @@
+public class Solution {
+    public double myPow(double x, int n) {
+        if(n == 0)
+            return 1;
+        else if(n == Integer.MIN_VALUE){
+            n = n+2;
+        }
+        if(n < 0){
+            x = 1/x;
+            n = -n;
+        }
+        return (n%2 == 0)?myPow(x*x, n/2):x*myPow(x*x, n/2);
+    }
+}
