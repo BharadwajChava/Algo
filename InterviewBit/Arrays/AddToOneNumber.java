d75e2595fa388943becb6467ab605dc425370a8a
Tejchava <tejchava@gmail.com>
2017-05-07 00:24:37 -0700

    Update AddToOneNumber.java

--- InterviewBit/Arrays/AddToOneNumber.java
+++ InterviewBit/Arrays/AddToOneNumber.java
@@ -2,7 +2,7 @@ import java.math.BigInteger;
 public class Solution {
 	public ArrayList<Integer> plusOne(ArrayList<Integer> a) {
 	    int r = 1;
-	    for(int i = a.size()-1; i >= 0; i++){
+	    for(int i = a.size()-1; i >= 0; i--){
 	        int x = a.get(i);
 	        x+=r;
 	        r = x/10;
@@ -11,7 +11,14 @@ public class Solution {
 	    if(r != 0)
 	        a.add(0, r);
 	    else{
-	        
+	        int i = 0;
+	        while(i < a.size()){
+	            if(a.get(i) == 0)
+	                a.remove(i);
+	            else
+	                break;
+	        }
 	    }
+	    return a;
 	}
 }
